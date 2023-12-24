e1ed5433d93b27c7609f68276830c8dabe5bb887
saicharan0123 <charansaicharan00@gmail.com>
2023-12-24 21:44:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+
+  function deleteHandler(i) {
+    let copyTask = [...tasks];
+    copyTask.splice(i, 1);
+    setTasks(copyTask);
+  }
+
+  let renderTask = <li><h3>No Task available </h3></li>
+
+  if (tasks.length > 0) {
+    renderTask = tasks.map((t, i) => {
+      return (
+        <>
+          <li key={i}>
+            <h3>{t.title}</h3>
+            <h3>{t.desc}</h3>
+            <button onClick={() => { deleteHandler(i) }}>Delete</button>
+          </li>
+        </>
+      );
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className='TaskInput'>
+        <input type='text' placeholder='Enter the Title' value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}>
+        </input>
+
+        <input type='text'id='descp' placeholder='Enter your Task' value={desc}
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}>
+        </input>
+
+        <button onClick={() => {
+          if(title != "" || desc != ""){
+            setTasks([...tasks, { title, desc }]);
+            setTitle("");
+            setDesc("");
+          }
+        }}>Add Task</button>
+      </div>
+      <div className='TaskAdded'>
+        <h1>Todays Tasks</h1>
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
     </div>
   );
 }
